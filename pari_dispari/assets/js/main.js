a6f2b595eb4279651aa396910db92525352b1606
Nicola-Perera <nicola.perera96@mail.com>
2021-10-20 00:24:51 +0200

    pari_dispari: add steps to build the function

---
+++ pari_dispari/assets/js/main.js
@@ -0,0 +1,19 @@
+/*
+    Pari e Dispari
+        L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. 
+        Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). 
+        Sommiamo i due numeri 
+        Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) 
+        Dichiariamo chi ha vinto.
+*/
+
+/*
+    FUNCTION:
+        la funzione accetta 2 parametri (una stringa e un numero);
+        si somma il numero inserito ad un numero random generato dal computer;
+        si stabilisce se la somma è un numero pari o dispari;
+        in caso sia pari creo una variabile contenente una stringa,
+        viceversa se è dispari creo una variabile con una stringa diversa;
+        se la variabile creata contiene la stringa uguale a quella inserita dall'utente come primo parametro della funzione, l'utente ha vinto il gioco;
+        se la stringhe sono diverse, il computer vince il gioco;
+*/
