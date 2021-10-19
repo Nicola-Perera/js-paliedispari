8f78c094c96f0c123daeb538b0244143d82fc6a5
Nicola-Perera <nicola.perera96@mail.com>
2021-10-19 23:42:48 +0200

    palindromo: add steps to build the function

---
+++ palindromo/assets/js/main.js
@@ -0,0 +1,14 @@
+/*
+PALINDROMA:
+    Chiedere all’utente di inserire una parola.
+    Creare una funzione per capire se la parola inserita è palindroma
+*/
+
+/*
+FUNCTION:
+    scompongo la parola nei vari caratteri che la compongono;
+    inserisco i caratteri in un array per riscrivere la parola al contrario;
+    paragono le 2 parole;
+    in caso di valore true, la parola è palindroma e restituisco il valore true;
+    in caso contrario restituisco valore false;
+*/
